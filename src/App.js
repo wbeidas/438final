37dcc9bf2a07619dfc821b8be3555c748e067aaa
Walid Beidas <walidbeidas@Walids-MacBook-Air.local>
2020-02-26 21:03:24 -0800

    making progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Calculator from "./components/Calculator";
+import InfoAboutApp from "./components/InfoAboutApp";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <InfoAboutApp/>
+        <div id="wrapper">
+          <div id="calculator-wrapper">
+            <Calculator/>
+          </div>
+        </div>
+      </div>);
+  }
 }
 
 export default App;
