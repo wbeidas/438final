9a155a362e0446d96ee5fc86110183758fbe5016
Walid Beidas <walidbeidas@Walids-MacBook-Air.local>
2020-03-08 17:39:38 -0700

    new updates to everything

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
-import React, {Component} from 'react';
-import Calculator from "./components/Calculator";
-import InfoAboutApp from "./components/InfoAboutApp";
+import React,{Component} from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <InfoAboutApp/>
-        <div id="wrapper">
-          <div id="calculator-wrapper">
-            <Calculator/>
-          </div>
-        </div>
-      </div>);
-  }
-}
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			languages : [
+				{name: "Pizza", votes: 0},
+				{name: "Burgers", votes: 0},
+				{name: "Tacos", votes: 0},
+				{name: "Salad", votes: 0}
+			]
+		}
+	}
 
+	vote (i) {
+		let newLanguages = [...this.state.languages];
+		newLanguages[i].votes++;
+		function swap(array, i, j) {
+			var temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+		this.setState({languages: newLanguages});
+		
+	}
+
+	render(){
+		return(
+			<>
+				<h1>Whats is your favorite food?</h1>
+				<div className="foods">
+					{
+						this.state.languages.map((lang, i) => 
+							<div key={i} className="language">
+								<div className="voteCount">
+									{lang.votes}
+								</div>
+								<div className="languageName">
+									{lang.name}
+								</div>
+								<button onClick={this.vote.bind(this, i)}>Click Here</button>
+							</div>
+						)
+					}
+				</div>
+			</>
+		);
+	}
+}
 export default App;
